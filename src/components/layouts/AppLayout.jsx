4c5ae3c7fe71e74c46868eb43e6f7bfb22ce2f03
Abhishek Sharma <108340138+Abhishek-img@users.noreply.github.com>
2026-04-03 17:39:38 +0530

    Update AppLayout.jsx

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
-import Footer from './footer'
 
 const AppLayout = () => {
     return (
         <>
             <Header />
             <Outlet />
-            <Footer />
         </>
     )
 }

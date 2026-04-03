3ada7ffc43d4c0d4099bb1033e5c66370c9da38e
Abhishek Sharma <108340138+Abhishek-img@users.noreply.github.com>
2026-04-03 17:43:19 +0530

    Update apiConstant.js

--- src/constant/apiConstant.js
+++ src/constant/apiConstant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://138.68.68.183:4001/api/v1";
+export const BASE_URL = "https://138.68.68.183:4001/api/v1";
 
 export const API_ROUTES = {
     register: "/user/register",
